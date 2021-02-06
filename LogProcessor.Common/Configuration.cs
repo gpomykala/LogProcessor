77613b55d10a131f19fb8ca5615e810479734d92
Grzegorz Pomykala <grzegorz.pomykala@gmail.com>
2021-02-06 22:57:58 +0100

    tests fixed

--- LogProcessor.Common/Configuration.cs
+++ LogProcessor.Common/Configuration.cs
@@ -6,8 +6,8 @@ namespace LogProcessor.Common
 {
     public class Configuration
     {
-        public int InsertBatchSize { get; set; }
-        public int MaxDegreeOfParallelism { get; set; }
+        public int InsertBatchSize { get; set; } = 5000;
+        public int MaxDegreeOfParallelism { get; set; } = 1;
         public string DbFileName { get; set; }
     }
 }
